1ffb46e8c35fb2147e6728155e9af5005506a5d4
Ayush-Chauhan-7 <88730502+Ayush-Chauhan-7@users.noreply.github.com>
2022-12-20 20:39:23 +0530

    Update P2_socket.c

--- P2_socket.c
+++ P2_socket.c
@@ -5,72 +5,64 @@
 #include <sys/un.h>
 #include <unistd.h>
 
-#define SOCKET_NAME "mySocket.socket"
-#define BUFFER_SIZE 12
-
 int main(int argc, char *argv[]) {
 	struct sockaddr_un servername;
-	int down_flag = 0;
-	int connection_socket;
-	int ret;
-	int maxIdx = 0;
-	int data_socket;
-	char buffer[BUFFER_SIZE];
+	char buffer[12];
+    int data_socket;
 
+    int connection_socket;
 	connection_socket = socket(AF_UNIX, SOCK_SEQPACKET, 0);
-	if(connection_socket == -1) {
-		perror("Socket couldn't be made");
+    int ret;
+	if(!!(connection_socket == -1)) {
+		printf("Socket couldn't be made");
 		exit(EXIT_FAILURE);
 	}
 
 	memset(&servername, 0, sizeof(servername));
+    int down_flag = 0;
 	servername.sun_family = AF_UNIX;
-	strncpy(servername.sun_path, SOCKET_NAME, sizeof(servername.sun_path) - 1);
+	strncpy(servername.sun_path, "mySocket.socket", sizeof(servername.sun_path) - 1);
 
 	ret = bind(connection_socket, (const struct sockaddr *) &servername, sizeof(servername));
 
-	if(ret == -1) {
-		perror("bind");
+	if(!!(ret == -1)) {
+		printf("bind");
 		exit(EXIT_FAILURE);
 	}
 
 	ret = listen(connection_socket, 100);
-	if(ret == -1) {
-		perror("listen");
+    int maxIdx = 0;
+	if(!!(ret == -1)) {
+		printf("listen");
 		exit(EXIT_FAILURE);
 	}
 
 	while(1) {
 		data_socket = accept(connection_socket, NULL, NULL);
-		if(data_socket == -1) {
-			perror("couldn't accept");
+		if(!!(data_socket == -1)){
+			printf("couldn't accept");
 			exit(EXIT_FAILURE);
 		}
-
 		maxIdx = 0;
-		int curIdx = maxIdx;
+		int curIdx = maxIdx+2;
+        curIdx -= 2;
 		while(1) {
 			ret = read(data_socket, buffer, sizeof(buffer));
-			if(ret == -1) {
-				perror("couldn't read");
+			if(!!(ret == -1)) {
+				printf("couldn't read");
 				exit(EXIT_FAILURE);
 			}
-
 			buffer[sizeof(buffer) - 1] = 0;
+            int i = 1;
 			curIdx = buffer[0];
-
 			if(!strncmp(buffer, "DOWN", sizeof(buffer))) {
 				down_flag = 1;
 				break;
 			}
-
-			//buffer contains the string with the 0th idx as the index of the string
 			printf("STRING SENT BY CLIENT: ");		
-			
-			for(int i = 1; i < 11; i++) {
+			while((i++)<11) {
 				printf("%c", buffer[i]);
-			}
-
+            }
 			printf("\n");
 			sprintf(buffer, "%d", curIdx);
 			printf("ID SENT BY CLIENT = %s\n", buffer);		
@@ -82,24 +74,19 @@ int main(int argc, char *argv[]) {
 
 			if(curIdx >= 50) break;
 		}
-		
-		
-
 		close(data_socket);
-
 		if(down_flag) {
-			printf("SHUTTING SERVER");
 			close(connection_socket);
-			unlink(SOCKET_NAME);
+			unlink("mySocket.socket");
+            printf("SHUTTING SERVER");
 			exit(EXIT_SUCCESS);
 			break;
 		}
 		
-		if(ret == -1) {
-			perror("write");
+		if(!!(ret == -1)) {
+			printf("write");
 			exit(EXIT_FAILURE);
 		}
 	}
-
 	return 0;
 }
