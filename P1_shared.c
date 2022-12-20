e44236db40277d72cede34f50dae05a74df0a7d4
Ayush-Chauhan-7 <88730502+Ayush-Chauhan-7@users.noreply.github.com>
2022-12-21 03:12:42 +0530

    Update P1_shared.c

--- P1_shared.c
+++ P1_shared.c
@@ -3,14 +3,33 @@
 #include<unistd.h>  
 #include<sys/shm.h>  
 #include<string.h>  
+#include <stdbool.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <time.h>
 
 int main()
 {
-    char *shared_memory;  
-    char buff[6] = "Hello";  
+    char *shared_memory;   
     int shmid;  
     shmid=shmget((key_t)2345, 1024, 0666|IPC_CREAT); 
-    shared_memory=(char *)shmat(shmid,NULL,0);  
-    strcpy(shared_memory,buff);
-    return 0; 
+    shared_memory=(char *)shmat(shmid,NULL,0);
+    char strings[50][7];
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (int i = 0; i < 50; i++) {
+        for (int j = 0; j < 6; j++) {
+            strings[i][j] = alphabet[rand() % 52];
+        }
+        strings[i][6]='\0';
+    }
+    for(int i=0;i<50;i+=5)
+    {
+        for(int j=0;j<5;j++)
+        {
+            strcpy(shared_memory,strings[i+j]);
+            while(str!='7');
+        }
+    }
+    return 0;
 }
