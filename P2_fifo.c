9fe5937c1dab61a67a523c13d0a677eb9b4030d1
Ayush-Chauhan-7 <88730502+Ayush-Chauhan-7@users.noreply.github.com>
2022-12-20 19:00:35 +0530

    Update P2_fifo.c

--- P2_fifo.c
+++ P2_fifo.c
@@ -1,33 +1,35 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdbool.h>
-#include <limits.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
 void sync(void);
-//client
-//P2 FIFO 
 struct pair {
-    int ID;
     char str_value[7];
+    int ID;
+    int flag;
 };
+
 int main()
 {
     mkfifo("/tmp/randomStrings_2020026_FIFO", 0666);
-    int i=0;
-    while ((i++)<10)
+    for(int i=0;i<10;i++)
     {
-        struct pair receiving[5];
         printf("Reading strings from FIFO file\n");
         int fd = open("/tmp/randomStrings_2020026_FIFO", O_RDONLY);
+        struct pair receiving[5];
         read(fd, receiving, sizeof(receiving));
-        struct pair maxIDstring;
         int maxID = INT_MIN;
-        for(int j=0; j<(sizeof(receiving)/sizeof(struct pair)); j++){
+        struct pair maxIDstring;
+        int j = 0;
+        while(j<(sizeof(receiving)/sizeof(struct pair))){
             printf("String ID: %d\n", receiving[j].ID);
             printf("String value: %s\n", receiving[j].str_value);
             if(receiving[j].ID>maxID){
@@ -35,9 +37,10 @@ int main()
                 maxIDstring.ID = maxID;
                 strncpy(maxIDstring.str_value, receiving[j].str_value, sizeof(receiving[j].str_value));                
             }
+            j++;
         }
-        close(fd);
         printf("Writing the string with highest ID into the FIFO file\n\n");
+        close(fd);
         fd = open("/tmp/randomStrings_2020026_FIFO", O_WRONLY);
         write(fd, &maxIDstring , sizeof(maxIDstring));
         close(fd);
